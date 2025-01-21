3364ab1be049823d9fdab3dc6711aa635a8e2bf4
Eduardo <eduardo.coltro@hotmail.com>
2025-01-21 11:21:03 -0300

    Adicionado client de email

---
+++ src/main/java/com/zenxdu/bffagendadortarefas/business/EmailService.java
@@ -0,0 +1,20 @@
+package com.zenxdu.bffagendadortarefas.business;
+
+
+import com.zenxdu.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.zenxdu.bffagendadortarefas.infrastructure.client.EmailClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final EmailClient emailClient;
+
+    public void enviaEmail(TarefasDTOResponse dto){
+        emailClient.enviarEmail(dto);
+    }
+
+}
+
